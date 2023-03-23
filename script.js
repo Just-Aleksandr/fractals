27ac4657581b3b2ace5d028794fd874d100526d3
BuildTools <unconfigured@null.spigotmc.org>
2023-03-23 01:08:09 -0400

    added functionality for sliders

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var angleSlider = document.getElementById("angle-slider");
+var lengthSlider = document.getElementById("length-slider");
+
+var angle = angleSlider.value;
+var length = lengthSlider.value;
+
 context.strokeStyle = "#008000";
 
-context.translate(canvas.width / 2, canvas.height);
-context.rotate(Math.PI);
-context.moveTo(0, 0);
 
 var tree = function (length) {
   if (length > 1) {
@@ -15,10 +18,10 @@ var tree = function (length) {
     context.stroke();
     context.closePath();
     context.save();
-    context.rotate((45 * Math.PI) / 180);
+    context.rotate((angle * Math.PI) / 180);
     tree(length * 0.67);
     context.restore();
-    context.rotate((-45 * Math.PI) / 180);
+    context.rotate((-angle * Math.PI) / 180);
     tree(length * 0.67);
   }
 };
@@ -27,5 +30,23 @@ var defaultTree = function () {
   tree(60);
 };
 
-// defaultTree();
-tree(120);
+var draw = function () {
+  context.setTransform(1, 0, 0, 1, 0, 0);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.translate(canvas.width / 2, canvas.height);
+  context.rotate(Math.PI);
+  context.moveTo(0, 0);
+  angle = angleSlider.value;
+  length = lengthSlider.value;
+  tree(length);
+}
+
+angleSlider.oninput = function () {
+  draw();
+}
+
+lengthSlider.oninput = function () {
+  draw();
+}
+
+draw();
